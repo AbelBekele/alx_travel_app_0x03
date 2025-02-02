7029cbd0065e4b3442fc9ff762d50a739c148d5f
Abel Bekele <abelbekele.addise@gmail.com>
2025-02-02 16:57:17 +0300

    Updating readme

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Listing, Booking
 from .serializers import ListingSerializer, BookingSerializer
+from .tasks import send_booking_confirmation_email
 
 class ListingViewSet(viewsets.ModelViewSet):
     """
@@ -20,6 +21,18 @@ class BookingViewSet(viewsets.ModelViewSet):
     """
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+
+    def perform_create(self, serializer):
+        booking = serializer.save()
+        
+        # Trigger the email task asynchronously
+        send_booking_confirmation_email.delay(
+            booking_id=booking.id,
+            user_email=booking.user.email,
+            listing_title=booking.listing.title
+        )
+        
+        return booking
 
 @api_view(['GET'])
 def sample_api(request):
