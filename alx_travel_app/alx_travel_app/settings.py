26c9cedc9e4200fd868b56a80f20d4ff6e838c86
Abel Bekele <abelbekele.addise@gmail.com>
2025-02-23 14:13:50 +0300

    Adding account registration

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'drf_yasg',
     'corsheaders',
     'listings',
+    'accounts',
 ]
 
 MIDDLEWARE = [
--- alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/urls.py
@@ -7,21 +7,22 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="ALX Travel API",
-      default_version='v1',
-      description="API documentation for ALX Travel Application",
-      terms_of_service="https://www.yourapp.com/terms/",
-      contact=openapi.Contact(email="contact@alxtravel.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
+    openapi.Info(
+        title="ALX Travel API",
+        default_version='v1',
+        description="API documentation for ALX Travel Application",
+        terms_of_service="https://www.yourapp.com/terms/",
+        contact=openapi.Contact(email="contact@alxtravel.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('listings.urls')),
+    path('api/accounts/', include('accounts.urls')),  # Add this line
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
